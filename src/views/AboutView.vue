30efcd0bcd6e032e8f4cc6c85bd78fe1d5b2fd41
WRH0000 <3484055326@qq.com>
2025-10-31 12:56:49 +0800

    第一次提交项目

---
+++ src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>关于我们</h1>
+    <p>这是 About 页面。</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
